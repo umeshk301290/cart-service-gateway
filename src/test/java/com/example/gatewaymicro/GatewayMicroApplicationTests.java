a9036310f91104038eea17d1b55b0981d463c4e3
umeshk301290 <55276171+umeshk301290@users.noreply.github.com>
2020-07-28 20:20:52 +0530

    Add files via upload

---
+++ src/test/java/com/example/gatewaymicro/GatewayMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaymicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
